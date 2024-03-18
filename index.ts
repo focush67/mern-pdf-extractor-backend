21fa88a3c910e4faea6b0cb92c70c630b7c91e20
Sparsh Verma <sparshv70@gmail.com>
2024-03-18 18:22:23 +0530

    Checking for all URLS

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log("Opl");
+import express from "express";
+import cors from "cors";
+import ApiRouter from "./routes/api/route";
+import cookieParser from "cookie-parser";
+import { config } from "dotenv";
+import { corsOptions } from "./utilities/cors-options";
+
+config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/", (_, response) => {
+  console.log("Home page accessed");
+  return response.json({
+    message: "Home Page accessed",
+    status: 201,
+  });
+});
+
+app.use("/api", ApiRouter);
+
+app.use("/files", express.static("files"));
+
+app.listen(4000, () => {
+  console.log("Server running at port 4000");
+});
