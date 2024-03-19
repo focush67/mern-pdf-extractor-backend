b745c68f60a2132ae9ce29f01ade92c33030235b
Sparsh Verma <sparshv70@gmail.com>
2024-03-19 20:22:02 +0530

    Adding render among allowed domains

--- utilities/allowed-origins.ts
+++ utilities/allowed-origins.ts
@@ -3,4 +3,5 @@ export const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3002",
   "https://mern-pdf-extractor-frontend.vercel.app",
+  "https://mern-pdf-extractor-frontend.onrender.com/",
 ];
